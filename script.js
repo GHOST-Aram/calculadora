8d6018c6f0d71209de7692ae14b36f6e53764b95
GHOST-Aram <owinoojala@gmail.com>
2022-11-27 12:15:08 +0300

    Create operate function

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ function subtract(num1, num2){
     return num1 - num2
 }
 
+//Operator method
+
+function operator(num1, num2, operator){
+    let answer = 0
+    switch(operator){
+        case operator === '+':
+            answer = add(num1, num2)
+            break
+        case operator === '-':
+            answer = subtract(num1, num2)
+            break
+        case operator === '*':
+            answer = multiply(num1, num2)
+            break
+        case operator === '/':
+            answer = divide(num1, num2)
+            break
+        case operator === '%':
+            answer = divMod(num1, num2)
+            break
+        default:
+            answer = 0
+    }
+    return answer
+}
